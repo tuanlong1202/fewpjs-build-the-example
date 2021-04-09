2ebb5495a901b540bfd82743170f65c250b5a594
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-09 16:53:08 -0400

    Build the example

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
 
+const shapeHearts = document.querySelectorAll(".like-glyph");
+const modal = document.getElementById('modal');
 
+for( let heart of shapeHearts) {
+    heart.addEventListener('click', likeClick);
+}
 
+function likeClick(e) {
+  let goal = e.target;
+  mimicServerCall()
+    .then(function(serverMessage) {
+      //
+      if (goal.innerText === FULL_HEART ) {
+        goal.className = 'like-glyph';
+        goal.innerText = EMPTY_HEART;
+      } else {
+        goal.className = 'activated-heart';
+        goal.innerText = FULL_HEART;
+      }
+    })
+    .catch(function(error) {
+      //
+      modal.className = '';
+      document.getElementById('modal-message').innerText = error;
+      setTimeout(hideModal,5e3);
+    })
+}
+
+function hideModal() {
+  modal.className = 'hidden';
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
